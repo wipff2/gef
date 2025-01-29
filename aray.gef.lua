834a270a488215936fa389d206120e4bbe115841
wipff2 <kl.comp.me@gmail.com>
2025-01-29 19:04:21 +0800

    Update aray.gef.lua

--- aray.gef.lua
+++ aray.gef.lua
@@ -3,10 +3,10 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Rayfield Example Window",
+   Name = "Deepmarian hb",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Rayfield Interface Suite",
-   LoadingSubtitle = "by Sirius",
+   LoadingTitle = "Rayfield Interface",
+   LoadingSubtitle = "by -",
    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
    DisableRayfieldPrompts = false,
